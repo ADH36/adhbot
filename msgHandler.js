84f77b1aca2ecc42d5c135e4693e395270f7ca3a
ADH <amritpalvirk36@gmail.com>
2021-04-11 00:04:13 +0530

    Open WA update

--- msgHandler.js
+++ msgHandler.js
@@ -1,4 +1,4 @@
-const { decryptMedia } = require('@open-wa/wa-decrypt')
+const { create, Client } = require('@open-wa/wa-automate')
 const fs = require('fs-extra')
 const axios = require('axios')
 const moment = require('moment-timezone')
